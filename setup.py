eabdfce15f8622ebe6e746ab4a7bd33cf76819bd
TestDotCom <calliari.alessio@gmail.com>
2020-04-29 12:05:59 +0200

    new: bump version for first official release

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='pirateplayer',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -21,9 +24,11 @@ setup(
             [console_scripts]
             pirateplayer=player:main
         ''',
+    description='Play offline music on PirateAudio',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/TestDotCom/pirateplayer',
     license='MIT License',
     author='TestDotCom',
-    author_email='',
-    description='Play offline music on PirateAudio'
+    author_email=''
 )
